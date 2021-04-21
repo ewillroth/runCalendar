9d398c662f23eb45b4bca554ed3efca9278b0103
Eric <eric.willroth@projekt202.com>
2021-04-21 08:25:21 -0500

    add placeholder instructions

--- src/app/Calendar.tsx
+++ src/app/Calendar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Popper } from '@material-ui/core';
+import { makeStyles, styled } from '@material-ui/core/styles';
 import Cell from './Cell';
 import { calculateCellDate } from '../utils/utils';
 import { IOptions } from './Options';
@@ -19,6 +19,22 @@ const useStyles = makeStyles({
 	},
 });
 
+const Placeholder = styled('div')({
+	marginTop: '24px',
+	display: 'flex',
+	flexDirection: 'column',
+	alignItems: 'center',
+	justifyContent: 'center',
+});
+
+const Heading = styled('h1')({
+	color: '#f50057',
+});
+
+const Text = styled('p')({
+	margin: '12px 0',
+});
+
 const Calendar = ({ workouts, startDate, setWorkouts, options, setOptions }: ICalendarProps) => {
 	const classes = useStyles();
 
@@ -34,9 +50,16 @@ const Calendar = ({ workouts, startDate, setWorkouts, options, setOptions }: ICa
 		const index = i.toString();
 		return <Cell key={date} index={index} date={date} workout={workout} editWorkout={editWorkout} />;
 	});
+	const placeholder = (
+		<Placeholder>
+			<Heading>Run Calendar</Heading>
+			<Text>Generate a calendar file with a popular marathon training plan or a custom plan</Text>
+			<Text>Select a plan above to begin or create a custom plan by changing the number of weeks.</Text>
+		</Placeholder>
+	);
 	return (
 		<Grid container spacing={3} justify='center' alignItems='center' className={classes.calendar}>
-			{cells}
+			{workouts.length > 0 ? cells : placeholder}
 		</Grid>
 	);
 };
